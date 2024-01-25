e82c6e7a62f0e2328164b88336acd9ea8fadf1aa
TechniKris <30842158+TechniKris@users.noreply.github.com>
2024-01-25 04:11:44 +0100

    Split out a basic Readme

---
+++ README.md
@@ -0,0 +1,10 @@
+-----------------
+DEPS:
+bash, coreutils (cp), sed,
+future: mid3v2
+
+-----------------
+KNOWN ISSUES:
+- will fail on song files with different case than specified in the beatmap file
+- will only extract a single song from multi-song mapsets
+- no incorrect input handling
--- songs-exporter.sh
+++ songs-exporter.sh
@@ -5,12 +5,6 @@
 se_version="0.0.1"
 
 # -----------------
-# KNOWN ISSUES:
-# - will fail on song files with different case than specified in the beatmap file
-# - will only extract a single song from multi-song mapsets
-# - no incorrect input handling
-
-# -----------------
 # OPTIONS:
 
 # tag files (default=true)
@@ -21,11 +15,6 @@ tag=true
 # allow for Unicode tags (default=false)  <---------------- WIP
 # ^ enable if you know Japanese KEKW
 useUnicode=false
-
-# -----------------
-# DEPS:
-# bash, coreutils (cp), sed,
-# future: mid3v2
 
 # -----------------
 # FUNCTIONS:
